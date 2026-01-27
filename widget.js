0677d4c0ffb79050f6d1de4a55f659e72bf1c050
Jacqode <45409429+Jacqode@users.noreply.github.com>
2026-01-27 10:50:28 +0100

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,3 +1,7 @@
+// JavaScript = Elektrikeren i køkkenet
+// Denne funktion sender en besked ned i kælderen (Cloudflare Worker)
+// om at brugeren har taget en pause.
+
 async function sendPause(userId, companyId) {
   return fetch("https://plugandpause-backend.jakobhelkjaer.workers.dev/pause", {
     method: "POST",
@@ -10,9 +14,10 @@ async function sendPause(userId, companyId) {
   });
 }
 
+// Når brugeren trykker på knappen (doneBtn),
+// så sender vi pausen og opdaterer displayet på widgeten.
+
 document.getElementById("doneBtn").onclick = async () => {
   await sendPause("JAKOB123", "TEST");
   document.getElementById("status").textContent = "Pause registreret!";
 };
-
-
